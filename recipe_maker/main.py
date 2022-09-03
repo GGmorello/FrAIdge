7643015d3682a8ab8365c8181bf1af34e9ecbc4b
Gabriele Morello <sgabbo99gmail.com>
2022-09-03 19:09:32 +0200

    FastAPI and Redis

--- recipe_maker/main.py
+++ recipe_maker/main.py
@@ -1,17 +1,33 @@
 import csv
 from recipe import Recipe
-def main():
+from fastapi import FastAPI
+import uvicorn
+import redis
+
+app = FastAPI()
+redis = redis.Redis(
+    host='localhost',
+    port=6379)
+
+
+@app.on_event('startup')
+async def startup_event():
     datafile = 'Recipes.csv'
-    ingredientsfile = 'ingredients'
-    outfile = 'output.json'
     dataset = csv.reader(datafile, delimiter=',')
-    ingredients = csv.reader(ingredientsfile, delimiter=',')
     next(dataset)
     recipes = []
     for count, row in enumerate(dataset):
         recipes.append(Recipe(row[0], row[2], row[1]))
+    redis.set('recipes', repr(recipes))
 
 
+@app.get("/")
+async def root(ingredients):
+    recipes = eval(redis.get('recipes'))
+    search()
+    return {"message": "Hello World"}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
