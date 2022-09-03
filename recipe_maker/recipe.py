62fb8749fe30aa4ac142040ccb923159205a398e
steffanopsathas <s.g.psathas@student.tudelft.nl>
2022-09-03 19:26:42 +0200

    Added Search function

--- recipe_maker/recipe.py
+++ recipe_maker/recipe.py
@@ -1,6 +1,19 @@
-import requests
-import json
 from urllib.request import urlopen
+
+
+def search(recipes, ingredients):
+    matches = []
+    for recipe in recipes:
+        ingredient_matches = 0
+        for ingredient in ingredients:
+            if ingredient in recipe.ingredients:
+                ingredient_matches += 1
+
+        matches.append((recipe, ingredient_matches))
+
+    matches.sort(key=lambda tup: tup[1])
+    return matches[:5]
+
 
 class Recipe:
     def __init__(self, name: str, ingredients: list, link: str):
