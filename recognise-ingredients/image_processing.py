b8dcec8bbce9cc9a5978bd863d7018b50df289a1
Dock Staal <dock.staal@hotmail.nl>
2022-09-04 09:30:08 +0200

    Fixed the factor bug and made life a bit prettier

--- recognise-ingredients/image_processing.py
+++ recognise-ingredients/image_processing.py
@@ -1,12 +1,12 @@
 from PIL import Image
-from google.cloud import vision
+#from google.cloud import vision
 import os
-
 
 def cropImage(file_name, locations):
     image = Image.open(file_name)
     width = image.width
     height = image.height
+    factor = 0.05
 
     left = locations[0].x
     right = locations[0].x
@@ -19,14 +19,11 @@ def cropImage(file_name, locations):
         top = min(top, vertex.y)
         bottom = max(bottom, vertex.y)
 
-    # cropped = image.crop(
-    #     (left * width * 0.9, top * height * 0.9,
-    #      right * width * 1.1, bottom * height * 1.1)
-    # )
-    factor = 0.05
-    cropped = image.crop(
-        (left * width * (1 - factor), top * height * (1 - factor),
-         right * width * (1 + factor), bottom * height * (1 + factor))
-    )
+    real_left = max(0, left * width * (1 - factor))
+    real_right = min(width, right * width * (1 + factor))
+    real_top = max(0, top * height * (1 - factor))
+    real_bottom = min(height, bottom * height * (1 + factor))
+
+    cropped = image.crop((real_left, real_top, real_right, real_bottom))
     cropped.show()
     return cropped
