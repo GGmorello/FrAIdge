c34bf218a3547944b3d871ea9bc0773f67e3aa1d
Gissa <G.vandeLinde@student.tudelft.nl>
2022-09-04 11:10:20 +0200

    add hardcoded filter for results

--- recognise-ingredients/main.py
+++ recognise-ingredients/main.py
@@ -27,22 +27,38 @@ def objectLocalization(file_name, factor, show=False):
         image=image).localized_object_annotations
 
     print('Number of objects found: {}'.format(len(objects)))
-
+    result = []
     for object_ in objects:
         # Crop object
         buffer_value = cropToBuffer(file_name, object_, factor, show)
         buffer_image = vision.Image(content=buffer_value)
 
-        response = client.label_detection(image=buffer_image)
-        labels = response.label_annotations
-        # print("I'm a: ")
-        print(f"labels thinks: {labels[0].description}")
-        # printObj(object_)
+        object_recognition = client.object_localization(
+            image=buffer_image).localized_object_annotations
+        for i in range(min(3, len(object_recognition))):
+            result.append(object_recognition[i].name)
+        # result.append(object_.name)
+    return set(result)
+
+
+def filterIngredients(set):
+    removables = {
+        'Food',
+        'Container',
+        'Animal',
+        'Clock',
+        'Fruit'
+    }
+
+    return set.difference(removables)
 
 
 def main():
-    file_name = os.path.abspath('images/fridge.jpg')
-    objectLocalization(file_name, 0.02, show=False)
+    file_name = os.path.abspath('images/stjoopid.jpg')
+    objects = objectLocalization(file_name, 0.02, show=True)
+    # Filter ingredients
+    ingredients = filterIngredients(objects)
+    print(ingredients)
 
 
 if __name__ == "__main__":
